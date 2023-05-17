6f50a9dfcd119f04eaafe5b58943cacb50e5d2ba
mahfuzurrahman4044 <mahfuzurrahman4044@gmail.com>
2023-05-18 01:34:16 +0600

    login page design

--- src/Account/Login.jsx
+++ src/Account/Login.jsx
@@ -2,14 +2,46 @@ import { Link } from "react-router-dom";
 
 
 const Login = () => {
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+    }
     return (
         <div>
-            <h3>
-                Log in page
-            </h3>
-            <h3>
-                <Link to="/signup">Sign Up</Link>
-            </h3>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <form onSubmit={handleLogin}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email:</span>
+                                    </label>
+                                    <input type="text" placeholder="email" name="email" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password:</span>
+                                    </label>
+                                    <input type="password" placeholder="password" name="password" className="input input-bordered" />
+                                </div>
+                                <p className="my-2">New to Car Doctor? Please <Link to="/signup" className="btn btn-primary">Sign Up</Link></p>
+                                <div className="form-control mt-6">
+                                    <button className="btn btn-primary">Login</button>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 };
